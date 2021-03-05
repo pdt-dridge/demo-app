18e996f5e1af3cb36d9aea82873db80ef4664b90
David Ridge <dridge@pagerduty.com>
2021-03-05 21:29:53 +1100

    added second template

--- src/templateList.js
+++ src/templateList.js
@@ -2,14 +2,14 @@ export function getTemplates() {
 
   return new Array(
     {
-      title: 'Date modified example',
-      description: 'Adds a timestamp indicating the last time the document modified.',
-      content: '<p>Last Modified: <time class="mdate">This will be replaced with the date modified.</time></p>'
-    },
-    {
       title: 'DBS Incident Management',
       description: 'Template for DBS Incident Management Status Updates',
       url: "template/dbsTemplate.html"
+    },
+    {
+      title: 'Westpac Incident Management',
+      description: 'Template for Westpac Incident Management Status Updates',
+      url: "template/westpacTemplate.html"
     }
   );
   
