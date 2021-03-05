35c1018b9f1b6b57ec11ec3297dfe6a1325e53c3
David Ridge <dridge@pagerduty.com>
2021-03-05 14:37:01 +1100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -39,33 +39,26 @@ class App extends React.Component {
                alignleft aligncenter alignright alignjustify | \
                help',
           template_mdate_format: '%d/%m/%Y : %H:%M',
+          template_selected_content_classes: 'selcontent selectedcontent',
           template_replace_values: {
-            username: 'Jack Black',
-            staffid: '991234',
-            inboth_username: 'Famous Person',
-            inboth_staffid: '2213',
+            businessService: 'todo: incident.businessService',
+            priority: 'todo: incident.priority',
+            severity: 'todo: incident.severity',
+            incidentSummary: 'todo: incident.summary',
+            previousUpdates: 'todo: getPrevUpdates()',
           },
           template_preview_replace_values: {
-            preview_username: 'Jack Black',
-            preview_staffid: '991234',
-            inboth_username: 'Famous Person',
-            inboth_staffid: '2213',
+            businessService: 'todo: incident.businessService',
+            priority: 'todo: incident.priority',
+            severity: 'todo: incident.severity',
+            incidentSummary: 'todo: incident.summary',
+            previousUpdates: 'todo: getPrevUpdates()',
           },
           templates : [
             {
               title: 'Date modified example',
               description: 'Adds a timestamp indicating the last time the document modified.',
               content: '<p>Last Modified: <time class="mdate">This will be replaced with the date modified.</time></p>'
-            },
-            {
-              title: 'Replace values example',
-              description: 'These values will be replaced when the template is inserted into the editor content.',
-              content: '<p>Name: {$username}, StaffID: {$staffid}</p>'
-            },
-            {
-              title: 'Replace values preview example',
-              description: 'These values are replaced in the preview, but not when inserted into the editor content.',
-              content: '<p>Name: {$preview_username}, StaffID: {$preview_staffid}</p>'
             },
             {
               title: 'DBS Incident Management',
